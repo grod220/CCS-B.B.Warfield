b1cd5d42dee42a265a0393288505fb9462b4b7e3
Gabe Rodriguez <gabe@spotify.com>
2018-04-03 21:24:55 +0200

    Nav reformating in progress

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import Link from "gatsby-link";
+import styled from "styled-components";
+import Img from "gatsby-image";
+require("typeface-open-sans");
+require("typeface-open-sans-condensed");
+
+const Container = styled.div`
+  position: absolute;
+  width: 70rem;
+  margin: 2.5rem auto 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: space-between;
+  z-index: 1000;
+  a {
+    text-decoration: none;
+    color: white;
+  }  
+`;
+
+const BrandName = styled.h1`
+  display: block;
+  font-family: Open Sans, Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 1.2rem;
+  letter-spacing: 0.3rem;
+`;
+
+const Nav = styled.ul`
+  list-style-type: none;
+  width: 34rem;
+  display: flex;
+  justify-content: space-between;
+  text-transform: uppercase;
+  font-family: Open Sans Condensed, Arial, Helvetica, sans-serif;
+  font-weight: 600;
+  letter-spacing: 0.12rem;
+  font-size: 0.875rem;
+  li {
+    display: inline;
+  }
+`;
+
+const Navigation = () => (
+  <Container>
+    <BrandName>
+      <Link to="/">Calvary Stockholm</Link>
+    </BrandName>
+    <Nav>
+      <li>
+        <Link to="/about">Sundays</Link>
+      </li>
+      <li>
+        <Link to="/whoweare">Who we are</Link>
+      </li>
+      <li>
+        <Link to="/calendar">Calendar</Link>
+      </li>
+      <li>
+        <Link to="/giving">Giving</Link>
+      </li>
+      <li>
+        <Link to="/getintouch">Get in touch</Link>
+      </li>
+    </Nav>
+  </Container>
+);
+
+export default Navigation;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,33 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 
-import Header from "../components/Header";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 import "./index.css";
 
-const TemplateWrapper = ({ children, location, data }) => (
+const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: "description", content: "Sample" },
-        { name: "keywords", content: "sample, something" }
-      ]}
-    />
-    <Header
-      isHomepage={location.pathname === "/"}
-      content={data.contentYaml}
-      dataSizes={data.hero.sizes}
-    />
-    <div
-      style={{
-        margin: "0 auto",
-        maxWidth: 960,
-        padding: "0px 1.0875rem 1.45rem",
-        paddingTop: 0
-      }}
-    >
-      {children()}
-    </div>
+    <Navigation />
+    {children()}
+    <Footer />
   </div>
 );
 
@@ -37,19 +19,3 @@ TemplateWrapper.propTypes = {
 };
 
 export default TemplateWrapper;
-
-export const query = graphql`
-  query HeaderQuery {
-    contentYaml {
-      mission {
-        pretext
-        mission_statement
-      }
-    }
-    hero: imageSharp(id: { regex: "/hero-night.jpg/" }) {
-      sizes(maxWidth: 1240) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-  }
-`;
