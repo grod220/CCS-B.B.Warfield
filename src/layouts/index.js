cf485177484dc13263ff13ad5adc410845911635
Gabe Rodriguez <gabe@spotify.com>
2018-04-04 23:37:18 +0200

    Added parallax support

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
-
+import { ParallaxProvider } from "react-scroll-parallax";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import "./index.css";
@@ -9,7 +9,7 @@ import "./index.css";
 const TemplateWrapper = ({ children }) => (
   <div>
     <Navigation />
-    {children()}
+    <ParallaxProvider>{children()}</ParallaxProvider>
     <Footer />
   </div>
 );
