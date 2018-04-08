616d14bdce4f64873cf0938d337395b45539243e
Gabe Rodriguez <gabe@spotify.com>
2018-04-09 00:02:38 +0200

    Config order fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
         display: "minimal-ui",
         icon: "src/images/cross-favicon.png"
       },
-      "gatsby-plugin-offline"
-    }
+    },
+    "gatsby-plugin-offline"
   ]
 };
