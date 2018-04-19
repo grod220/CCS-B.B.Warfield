5cf07ddd26c55cb6a8fc9aa17cdb80f39c2bde37
Gabe Rodriguez <gabe@spotify.com>
2018-04-19 13:47:30 +0300

    static worship image added above footer

--- src/components/Homepage/preFooterImg.js
+++ src/components/Homepage/preFooterImg.js
@@ -1,15 +1,37 @@
 import React from "react";
 import styled from "styled-components";
+import { Parallax } from "react-scroll-parallax";
+import { isMobile } from "react-device-detect";
+
+import WorshipImg from "../../images/homepage-worship.jpg";
 
 const Container = styled.div`
+  height: 31.25rem;
   width: 100vw;
-  height: 500px;
   background-color: #dedede;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  position: relative;
+  overflow: hidden;
 `;
 
-const PreFooterImg = () => <Container>HERE IS AN INSPIRATIONAL IMG</Container>;
+const WithBackground = styled.div`
+  background: url('${WorshipImg}');
+  width: 100vw;
+  height: 37.5rem;
+  background-size: cover;
+  background-position: center;
+`;
+
+const PreFooterImg = () => (
+  <Container>
+    <Parallax
+      offsetYMin={-40}
+      offsetYMax={20}
+      slowerScrollRate
+      disabled={isMobile}
+    >
+      <WithBackground />
+    </Parallax>
+  </Container>
+);
 
 export default PreFooterImg;
