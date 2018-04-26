741f41fc653423771646bcb956704da4ba95369c
Gabe Rodriguez <gabe@spotify.com>
2018-04-26 13:55:51 +0200

    Testing error

--- lambda/googleSheets.js
+++ lambda/googleSheets.js
@@ -3,23 +3,23 @@
 import { google } from "googleapis";
 
 const authorize = async base64Keys => {
-  const client = await google.auth.getClient({
-    credentials: JSON.parse(
-      Buffer.from(base64Keys, "base64").toString("ascii")
-    ),
-    scopes: "https://www.googleapis.com/auth/spreadsheets"
-  });
-  return google.sheets({
-    version: "v4",
-    auth: client
-  });
+    const client = await google.auth.getClient({
+      credentials: JSON.parse(
+        Buffer.from(base64Keys, "base64").toString("ascii")
+      ),
+      scopes: "https://www.googleapis.com/auth/spreadsheets"
+    });
+    return google.sheets({
+      version: "v4",
+      auth: client
+    });
 };
 
 const addToCol = (sheets, range, emailAddress) => {
   return new Promise((resolve, reject) => {
     sheets.spreadsheets.values.append(
       {
-        spreadsheetId: "1PyITnQGRqwbYcsXIZNC2sANFlmKrY3SIgV7wKGW3X88",
+        spreadsheetId: "XTSDFSDFDFS",
         range,
         valueInputOption: "USER_ENTERED",
         resource: {
@@ -37,12 +37,14 @@ const addToCol = (sheets, range, emailAddress) => {
   });
 };
 
+// 1PyITnQGRqwbYcsXIZNC2sANFlmKrY3SIgV7wKGW3X88
+
 exports.handler = async function(event, context, callback) {
   try {
     const sheets = await authorize(process.env.GOOGLE_SERVICE_ACCOUNT);
     const res = await addToCol(sheets, "Local!F2", JSON.parse(event.body).email);
     return { statusCode: res.status, body: JSON.stringify(res.data) };
   } catch (err) {
-    return { statusCode: 500, body: err };
+    return { statusCode: 500, body: err.toString() };
   }
 };
