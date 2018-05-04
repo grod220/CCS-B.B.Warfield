5bfb3e46e20c44eb1784611590819c57e91a11ff
Gabe Rodriguez <gabe@spotify.com>
2018-05-04 13:23:22 +0200

    Who we are templates in place

--- src/components/whoWeAre/biblePromo.js
+++ src/components/whoWeAre/biblePromo.js
@@ -1,15 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
-  width: 100vw;
-  height: 500px;
-  background-color: gray;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+import BiblePic from "../../images/who-we-are-bible.jpg";
+import FeatureBlock from "../shared/featureWithBackground";
+
+const WhiteAnchor = styled.a`
+  color: white;
 `;
 
-const BiblePromo = () => <Container>BIBLE PROMO</Container>;
+const BiblePromo = () => (
+  <FeatureBlock image={BiblePic} disablePL>
+    We are passionate about the bible.
+    <br />
+    <WhiteAnchor
+      href="https://www.youtube.com/watch?v=LR4WfUCNfX4"
+      target="_blank"
+    >
+      Here’s a great explanation why →
+    </WhiteAnchor>
+  </FeatureBlock>
+);
 
 export default BiblePromo;
--- src/components/whoWeAre/churchFamilyPic.js
+++ src/components/whoWeAre/churchFamilyPic.js
@@ -1,15 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
-  width: 100vw;
-  height: 500px;
-  background-color: gray;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import ChurchPic from "../../images/who-we-are-inside-church.jpg";
+import FeatureBlock from "../shared/featureWithBackground";
 
-const ChurchFamilyPic = () => <Container>HERE ARE ChurchFamilyPic</Container>;
+const ChurchFamilyPic = () => <FeatureBlock image={ChurchPic} />;
 
 export default ChurchFamilyPic;
