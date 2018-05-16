74261d32c9a7c009dfe513abb29c6a4d3beb2938
Gabe Rodriguez <gabe@spotify.com>
2018-05-16 23:15:15 +0200

    Forgot the utils!

---
+++ src/components/shared/eventsUtil.js
@@ -0,0 +1,51 @@
+export const generateColor = (name, localObj) => {
+  const colorObj =
+    typeof window !== "undefined" && window.sessionStorage
+      ? window.sessionStorage
+      : localObj;
+  const refColors = [
+    "#ef5350",
+    "#EC407A",
+    "#AB47BC",
+    "#7E57C2",
+    "#5C6BC0",
+    "#42A5F5",
+    "#29B6F6",
+    "#26C6DA",
+    "#26A69A",
+    "#66BB6A",
+    "#9CCC65",
+    "#FFCA28",
+    "#FFA726",
+    "#FF7043",
+    "#8D6E63",
+    "#78909C"
+  ];
+  if (colorObj[name]) {
+    return colorObj[name];
+  } else {
+    const usedColors = Object.values(colorObj);
+    const leftOverColors = refColors.filter(
+      refColor => !usedColors.includes(refColor)
+    );
+
+    let randomIndex;
+    let randomColor;
+    if (leftOverColors.length) {
+      randomIndex = Math.floor(Math.random() * leftOverColors.length);
+      randomColor = leftOverColors[randomIndex];
+    } else {
+      randomIndex = Math.floor(Math.random() * refColors.length);
+      randomColor = refColors[randomIndex];
+    }
+    colorObj[name] = randomColor;
+    return randomColor;
+  }
+};
+
+export const removeOldEvents = eventsArr => {
+  const twentyFourHours = 1000 * 60 * 60 * 24;
+  return eventsArr.filter(
+    event => Date.parse(event.date) + twentyFourHours > Date.now()
+  );
+};
