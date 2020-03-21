cd752402be2112319cde29d2d12284b0c118b8f5
Gabe Rodriguez <gabe@spotify.com>
2020-03-21 15:00:21 +0100

    coronavirus popup notice

--- src/components/homepage/homePopup.js
+++ src/components/homepage/homePopup.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactModal from 'react-modal'
 import styled from 'styled-components'
 
-const IS_LIVE_UNTIL = new Date(2019, 2, 25)
+const IS_LIVE_UNTIL = new Date(2020, 4, 1)
 
 const AlertTitle = styled.h2`
   font-family: Open Sans, Arial, Helvetica, sans-serif;
@@ -21,8 +21,7 @@ const BlueButton = styled.button`
   font-family: Open Sans, Arial, Helvetica, sans-serif;
   color: #ffffff;
   background-color: #4473f6;
-  box-shadow: inset 0 -0.6em 1em -0.35em rgba(0, 0, 0, 0.17),
-    inset 0 0.6em 2em -0.3em rgba(255, 255, 255, 0.15),
+  box-shadow: inset 0 -0.6em 1em -0.35em rgba(0, 0, 0, 0.17), inset 0 0.6em 2em -0.3em rgba(255, 255, 255, 0.15),
     inset 0 0 0em 0.05em rgba(255, 255, 255, 0.12);
   text-align: center;
 
@@ -51,12 +50,18 @@ class HomePopup extends React.Component {
               overlay: {
                 backgroundColor: 'rgba(12, 12, 12, 0.75)',
                 zIndex: '1000',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
               },
               content: {
                 maxWidth: '500px',
-                height: 'auto',
-                maxHeight: 'fit-content',
-                margin: 'auto',
+                top: 'unset',
+                bottom: 'unset',
+                right: 'unset',
+                left: 'unset',
+                maxHeight: 'calc(100vh - 2rem)',
+                overflowY: 'auto',
                 display: 'flex',
                 alignItems: 'center',
                 flexDirection: 'column',
@@ -66,30 +71,21 @@ class HomePopup extends React.Component {
             }}
           >
             <AlertTitle>
-              Sunday Service alert{' '}
+              Notice{' '}
               <span role="img" aria-label="church">
                 ⛪
               </span>
             </AlertTitle>
             <AlertBody>
               <p>
-                This upcoming Sunday (March 24th), we will <b>NOT</b> be meeting
-                at our usual location.
+                In order to live out our mandate to <i>love others</i>, our congregation will suspend all general
+                gatherings (Sunday worship, Bible studies, and public prayer meetings) until further notice. This is
+                done in order to reduce potential spread of the corona virus. We are praying that God’s grace will
+                abound in the midst of this present sickness and suffering.
               </p>
               <p>
-                We'll be in{' '}
-                <a
-                  href="https://goo.gl/maps/DgXRBu4BHf62"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Lida friluftsgård
-                </a>
-                . Take a drive{' '}
-                <span role="img" aria-label="car">
-                  🚗
-                </span>{' '}
-                and come by! Reach out to Joe or Ursula for more details.
+                Contact: <a href="mailto:calvarystockholm@gmail.com">calvarystockholm@gmail.com</a> if you seek
+                assistance or prayer.
               </p>
             </AlertBody>
             <BlueButton onClick={this.handleCloseModal}>Close</BlueButton>
