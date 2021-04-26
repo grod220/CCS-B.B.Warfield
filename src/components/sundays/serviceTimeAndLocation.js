5a4cc640fb51b38d3d1a643f4ec2fb28168a3dde
Gabe Rodriguez <gabe@spotify.com>
2021-04-26 10:50:27 +0200

    Updating location

--- src/components/sundays/serviceTimeAndLocation.js
+++ src/components/sundays/serviceTimeAndLocation.js
@@ -38,14 +38,14 @@ const ServiceTimeAndLocation = () => (
   <TwoHalfWhiteBlock>
     <LeftDivFix>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2034.4261104425425!2d18.061877616317492!3d59.34253468166352!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d69af33a1c7%3A0xc00a805f698b8ae5!2sBirger+Jarlsgatan+66B%2C+114+29+Stockholm!5e0!3m2!1sen!2sse!4v1525357713147"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2034.4975008107906!2d18.0825308!3d59.3413429!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d44317956af%3A0x264cf8d1e4fa691f!2sNybrogatan%2079B%2C%20114%2041%20Stockholm%2C%20Sweden!5e0!3m2!1sen!2sde!4v1619426728178!5m2!1sen!2sde"
         width="100%"
         height="400"
         frameBorder="0"
         title="Map location of Calvary Stockholm"
       />
       <MapsAnchor
-        href="https://www.google.com/maps?ll=59.342568,18.064013&z=16&t=m&hl=en-US&gl=SE&mapclient=embed&q=Birger+Jarlsgatan+66B+114+29+Stockholm"
+        href="https://goo.gl/maps/B5uYD1Tn7VsyYUsv7"
         target="_blank"
       >
         <BoxLink>View in Google Maps</BoxLink>
@@ -53,13 +53,8 @@ const ServiceTimeAndLocation = () => (
     </LeftDivFix>
     <div>
       <Title>Service Time & Location</Title>
-      <p>Sundays @ 3:00pm </p>
-      <p>Korskyrkan’s Café, Birger Jarlsgatan 66B, 114 29 Stockholm</p>
-      <p>
-        It's only 5 min walk away from Rådmansgatan subway station (green line).
-        Above is a picture of the outside. It’s not the main entrance, but the
-        door to its left (look for 66B).
-      </p>
+      <p>Sundays @ 10:00am </p>
+      <p>Nybrogatan 79B, 114 41 Stockholm</p>
     </div>
   </TwoHalfWhiteBlock>
 );
